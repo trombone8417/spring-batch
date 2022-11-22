6640ea3cb5a8c70e690ac2d79e1dce27332211c3
trombone8417 <trombone8417@yahoo.com>
2022-11-22 19:15:35 +0800

    First Spring Batch Job with Tasklet Step

--- src/main/java/com/infybuzz/app/SpringBatchApplication.java
+++ src/main/java/com/infybuzz/app/SpringBatchApplication.java
@@ -1,9 +1,13 @@
 package com.infybuzz.app;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableBatchProcessing
+@ComponentScan("com.infybuzz.config")
 public class SpringBatchApplication {
 
 	public static void main(String[] args) {
