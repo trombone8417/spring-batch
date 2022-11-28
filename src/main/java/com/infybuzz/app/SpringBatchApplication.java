2bf68466b82e58b36e2ab04a0a8b6a18f4e7d4e1
trombone8417 <trombone8417@yahoo.com>
2022-11-28 21:06:07 +0800

    Start Job with REST API

--- src/main/java/com/infybuzz/app/SpringBatchApplication.java
+++ src/main/java/com/infybuzz/app/SpringBatchApplication.java
@@ -4,12 +4,14 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableBatchProcessing
 @ComponentScan({"com.infybuzz.config", "com.infybuzz.service",
 	"com.infybuzz.listener", "com.infybuzz.reader", "com.infybuzz.processor",
 	"com.infybuzz.writer", "com.infybuzz.controller"})
+@EnableAsync
 public class SpringBatchApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/infybuzz/controller/JobController.java
+++ src/main/java/com/infybuzz/controller/JobController.java
@@ -18,35 +18,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.infybuzz.service.JobService;
+
 @RestController
 @RequestMapping("/api/job")
 public class JobController {
 
 	@Autowired
-	JobLauncher jobLauncher;
-	
-	@Qualifier("firstJob")
-	@Autowired
-	Job firstJob;
-	
-	@Qualifier("secondJob")
-	@Autowired
-	Job secondJob;
+	JobService jobService;
 	
 	@GetMapping("/start/{jobName}")
 	public String startJob(@PathVariable String jobName) throws Exception {
-		
-		Map<String, JobParameter> params = new HashMap<>();
-		params.put("currentTime", new JobParameter(System.currentTimeMillis()));
-		
-		JobParameters jobParameters = new JobParameters(params);
-		
-		if (jobName.equals("First Job")) {
-			jobLauncher.run(firstJob, jobParameters);
-		} else if(jobName.equals("Second Job")) {
-			jobLauncher.run(secondJob, jobParameters);
-		}
-		
+		jobService.startJob(jobName);
 		return "Job Started...";
 	}
 	
