e5ec03e07a56a35453610ff39986811ff1e90a6b
trombone8417 <trombone8417@yahoo.com>
2022-11-29 21:00:58 +0800

    Scheduling Job with Spring Scheduler

--- src/main/java/com/infybuzz/app/SpringBatchApplication.java
+++ src/main/java/com/infybuzz/app/SpringBatchApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableBatchProcessing
@@ -12,6 +13,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 	"com.infybuzz.listener", "com.infybuzz.reader", "com.infybuzz.processor",
 	"com.infybuzz.writer", "com.infybuzz.controller"})
 @EnableAsync
+@EnableScheduling
 public class SpringBatchApplication {
 
 	public static void main(String[] args) {
