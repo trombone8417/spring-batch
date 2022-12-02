9ebf209abaf8c2a20a816c446d8af78998507a6e
trombone8417 <trombone8417@yahoo.com>
2022-12-03 07:35:06 +0800

    Create Flat File Item Reader with CSV File

--- src/main/java/com/infybuzz/config/SampleJob.java
+++ src/main/java/com/infybuzz/config/SampleJob.java
@@ -1,5 +1,7 @@
 package com.infybuzz.config;
 
+import java.io.File;
+
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
@@ -8,13 +10,19 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
 
 import com.infybuzz.listener.FirstJobListener;
 import com.infybuzz.listener.FirstStepListener;
+import com.infybuzz.model.StudentCsv;
 import com.infybuzz.processor.FirstItemProcessor;
 import com.infybuzz.reader.FirstItemReader;
 import com.infybuzz.service.SecondTasklet;
@@ -105,13 +113,49 @@ public class SampleJob {
 	
 	private Step firstChunkStep() {
 		return stepBuilderFactory.get("First Chunk Step")
-				.<Integer, Long>chunk(3)
-				.reader(firstItemReader)
+				.<StudentCsv, StudentCsv>chunk(3)
+				.reader(flatFileItemReader())
 				//.processor(firstItemProcessor)
 				.writer(firstItemWriter)
 				.build();
 	}
+	
+	public FlatFileItemReader<StudentCsv> flatFileItemReader() {
+		FlatFileItemReader<StudentCsv> flatFileItemReader =
+				new FlatFileItemReader<StudentCsv>();
+		
+		flatFileItemReader.setResource(new FileSystemResource(
+				new File("C:\\Users\\kuei\\Desktop\\github\\spring-batch\\InputFiles\\students.csv")));
+	
+		flatFileItemReader.setLineMapper(new DefaultLineMapper<StudentCsv>() {
+			{
+				setLineTokenizer(new DelimitedLineTokenizer() {
+					{
+						setNames("ID", "First Name", "Last Name", "Email");
+					}
+				});
+				
+				setFieldSetMapper(new BeanWrapperFieldSetMapper<StudentCsv>() {
+					{
+						setTargetType(StudentCsv.class);
+					}
+				});
+			}
+		});
+		
+		flatFileItemReader.setLinesToSkip(1);
+		
+		return flatFileItemReader;
+	}
 }
+
+
+
+
+
+
+
+
 
 
 
--- src/main/java/com/infybuzz/writer/FirstItemWriter.java
+++ src/main/java/com/infybuzz/writer/FirstItemWriter.java
@@ -5,11 +5,13 @@ import java.util.List;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.stereotype.Component;
 
+import com.infybuzz.model.StudentCsv;
+
 @Component
-public class FirstItemWriter implements ItemWriter<Long> {
+public class FirstItemWriter implements ItemWriter<StudentCsv> {
 
 	@Override
-	public void write(List<? extends Long> items) throws Exception {
+	public void write(List<? extends StudentCsv> items) throws Exception {
 		System.out.println("Inside Item Writer");
 		items.stream().forEach(System.out::println);		
 	}
