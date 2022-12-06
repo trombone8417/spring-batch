47a15dc784870eb6d44eda9cacc11ebfb8b5a973
trombone8417 <trombone8417@yahoo.com>
2022-12-06 20:45:29 +0800

    Create JDBC Item Reader with MySQL Database

--- src/main/java/com/infybuzz/config/SampleJob.java
+++ src/main/java/com/infybuzz/config/SampleJob.java
@@ -2,6 +2,8 @@ package com.infybuzz.config;
 
 import java.io.File;
 
+import javax.sql.DataSource;
+
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
@@ -11,6 +13,7 @@ import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
@@ -24,11 +27,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
 import com.infybuzz.listener.FirstJobListener;
 import com.infybuzz.listener.FirstStepListener;
 import com.infybuzz.model.StudentCsv;
+import com.infybuzz.model.StudentJdbc;
 import com.infybuzz.model.StudentJson;
 import com.infybuzz.model.StudentXml;
 import com.infybuzz.processor.FirstItemProcessor;
@@ -62,6 +67,9 @@ public class SampleJob {
 	
 	@Autowired
 	private FirstItemWriter firstItemWriter;
+	
+	@Autowired
+	private DataSource datasource;
 
 	@Bean
 	public Job firstJob() {
@@ -121,10 +129,11 @@ public class SampleJob {
 	
 	private Step firstChunkStep() {
 		return stepBuilderFactory.get("First Chunk Step")
-				.<StudentXml, StudentXml>chunk(3)
+				.<StudentJdbc, StudentJdbc>chunk(3)
 				//.reader(flatFileItemReader(null))
 				//.reader(jsonItemReader(null))
-				.reader(staxEventItemReader(null))
+				//.reader(staxEventItemReader(null))
+				.reader(jdbcCursorItemReader())
 				//.processor(firstItemProcessor)
 				.writer(firstItemWriter)
 				.build();
@@ -193,6 +202,24 @@ public class SampleJob {
 		});
 		
 		return staxEventItemReader;
+	}
+	
+	public JdbcCursorItemReader<StudentJdbc> jdbcCursorItemReader(){
+		JdbcCursorItemReader<StudentJdbc> jdbcCursorItemReader = 
+				new JdbcCursorItemReader<StudentJdbc>();
+		
+		jdbcCursorItemReader.setDataSource(datasource);
+		jdbcCursorItemReader.setSql(
+				"select id, first_name as firstName, last_name as lastName,"
+				+ "email from student");
+		
+		jdbcCursorItemReader.setRowMapper(new BeanPropertyRowMapper<StudentJdbc>() {
+			{
+				setMappedClass(StudentJdbc.class);
+			}
+		});
+		
+		return jdbcCursorItemReader;
 	}
 }
 
--- src/main/java/com/infybuzz/writer/FirstItemWriter.java
+++ src/main/java/com/infybuzz/writer/FirstItemWriter.java
@@ -6,14 +6,15 @@ import org.springframework.batch.item.ItemWriter;
 import org.springframework.stereotype.Component;
 
 import com.infybuzz.model.StudentCsv;
+import com.infybuzz.model.StudentJdbc;
 import com.infybuzz.model.StudentJson;
 import com.infybuzz.model.StudentXml;
 
 @Component
-public class FirstItemWriter implements ItemWriter<StudentXml> {
+public class FirstItemWriter implements ItemWriter<StudentJdbc> {
 
 	@Override
-	public void write(List<? extends StudentXml> items) throws Exception {
+	public void write(List<? extends StudentJdbc> items) throws Exception {
 		System.out.println("Inside Item Writer");
 		items.stream().forEach(System.out::println);		
 	}
